bb2a2279c9a0f1bacf8ae233aaa076e89a1b4736
Jovansonlee Cesar <ivanceras@gmail.com>
2024-07-06 12:46:46 +0800

    fix: and remove warnings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
+//#![deny(warnings)]
 use bevy::core_pipeline::experimental::taa::TemporalAntiAliasBundle;
 use bevy::core_pipeline::experimental::taa::TemporalAntiAliasPlugin;
-use bevy::diagnostic::DiagnosticsStore;
-use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 use bevy::math::primitives::Cuboid;
 use bevy::math::*;
-use bevy::pbr::wireframe::WireframeConfig;
-use bevy::pbr::ExtendedMaterial;
 use bevy::prelude::*;
-use bevy::render::primitives::Aabb;
 use bevy::window::PresentMode;
-use bevy::window::PrimaryWindow;
-use bevy::winit::WinitWindows;
 use bevy_obj::ObjPlugin;
 use avian3d::prelude::*;
-use bevy::color::palettes::basic::*;
 
 #[derive(Resource, Default)]
 struct Obj {
@@ -84,7 +77,6 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
 ) {
     commands.spawn((DirectionalLightBundle {
         directional_light: DirectionalLight {
@@ -132,7 +124,7 @@ fn setup(
         Collider::convex_hull_from_mesh(&ground_mesh).unwrap(),
         PbrBundle {
             mesh: meshes.add(ground_mesh),
-            material: materials.add(Color::rgba(0.7, 0.7, 0.8, 0.5)),
+            material: materials.add(Color::srgba(0.7, 0.7, 0.8, 0.5)),
             transform: Transform {
                 translation: vec3(0.0, -2.0, 0.0),
                 ..default()
@@ -144,7 +136,7 @@ fn setup(
     let side_x_collider =
         Collider::convex_hull_from_mesh(&side_x_mesh).unwrap();
     let side_x_handle = meshes.add(side_x_mesh);
-    let westrac = materials.add(Color::rgb(1.0, 0.8, 0.067));
+    let westrac = materials.add(Color::srgb(1.0, 0.8, 0.067));
     // Side X
     commands.spawn((
         RigidBody::Static,
@@ -195,26 +187,24 @@ fn setup(
 
 fn spawn_obj(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
+    meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
     obj: Res<Obj>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Space) {
 
         let colors = vec![
-            Color::rgb(1.0, 0.0, 0.0), //red
-            Color::rgb(0.0, 1.0, 0.0), //blue
-            Color::rgb(1.0, 0.08, 0.58), //pink
-            Color::rgb(1.0, 0.65, 0.0), //orange
-            Color::rgb(0.5, 0.0, 0.5), // purple
-            Color::rgb(0.0, 1.0, 1.0), //cyan
+            Color::srgb(1.0, 0.0, 0.0), //red
+            Color::srgb(0.0, 1.0, 0.0), //blue
+            Color::srgb(1.0, 0.08, 0.58), //pink
+            Color::srgb(1.0, 0.65, 0.0), //orange
+            Color::srgb(0.5, 0.0, 0.5), // purple
+            Color::srgb(0.0, 1.0, 1.0), //cyan
         ];
 
         for (i, handle) in obj.handles.iter().enumerate() {
             let mesh = meshes.get(handle).unwrap();
-            let aabb = mesh.compute_aabb().unwrap();
 
             if let Some(collider) = Collider::convex_hull_from_mesh(&mesh) {
                 commands.spawn((
